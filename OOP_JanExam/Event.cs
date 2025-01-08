2bfe0cd1eb733116767298a991df1930e1c9914d
Calvin Regan <S00250043@atu.ie>
2025-01-08 13:20:01 +0000

    Added class Event

---
+++ OOP_JanExam/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_JanExam
+{
+    public enum EventType { Music, Comedy, Theatre }
+    internal class Event
+    {
+        public string Name { get; set; }
+        public DateTime EventDate { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public EventType TypeOfEvent { get; set; }
+    }
+}
+
