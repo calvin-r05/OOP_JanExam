de3fffec6e5d0c6a7058d9ba0e460fad176eb6fa
Calvin Regan <S00250043@atu.ie>
2025-01-08 13:11:36 +0000

    Created ticket class

---
+++ OOP_JanExam/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_JanExam
+{
+    internal class Ticket
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int AvailableTickets { get; set; }
+    }
+}
