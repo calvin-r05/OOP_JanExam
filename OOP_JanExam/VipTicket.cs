1739d5124ef398e17c18a775798993f0bb3dadce
Calvin Regan <S00250043@atu.ie>
2025-01-08 13:13:12 +0000

    Added extended class VIPTicket

---
+++ OOP_JanExam/VipTicket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_JanExam
+{
+    internal class VipTicket : Ticket
+    {
+        public string AdditionalExtras { get; set; }
+
+        public decimal AdditionalCost { get; set; }
+    }
+}
