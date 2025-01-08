d29ec5981fed529823ae5e528f1ea6063289c50c
Calvin Regan <S00250043@atu.ie>
2025-01-08 13:32:25 +0000

    Added IComparable functionality

--- OOP_JanExam/Event.cs
+++ OOP_JanExam/Event.cs
@@ -8,12 +8,28 @@ using System.Threading.Tasks;
 namespace OOP_JanExam
 {
     public enum EventType { Music, Comedy, Theatre }
-    internal class Event
+    internal class Event : IComparable<Event>
     {
+        #region properties
         public string Name { get; set; }
         public DateTime EventDate { get; set; }
         public List<Ticket> Tickets { get; set; }
         public EventType TypeOfEvent { get; set; }
+        #endregion properties
+
+        public Event(string name, DateTime eventDate, List<Ticket> tickets, EventType typeOfEvent) 
+        {
+            Name = name;
+            EventDate = eventDate;
+            Tickets = tickets;
+            TypeOfEvent = typeOfEvent;
+        }
+
+        //Used to sort a list of Event items by day of month
+        public int CompareTo(Event other)
+        {
+            return this.EventDate.Day.CompareTo(other.EventDate.Day);
+        }
     }
 }
 
